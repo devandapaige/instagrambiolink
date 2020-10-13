bc2cd2c01514622c4d65fc8ac9ec3a3be182d823
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-13 17:58:14 -0400

    social media icons array data added to index.js

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
 const socialLinks = [
-  { account: "", svgIcon: "", link: "" },
-  { account: "", svgIcon: "", link: "" },
-  { account: "", svgIcon: "", link: "" },
-  { account: "", svgIcon: "", link: "" },
-  { account: "", svgIcon: "", link: "" },
+  {
+    account: "LinkedIn",
+    svgIcon: "",
+    link: "https://www.linkedin.com/in/anelson36/",
+    color: "#0077B5",
+  },
+  {
+    account: "GitHub",
+    svgIcon: "",
+    link: "https://github.com/devandapaige",
+    color: "#24292E",
+  },
+  {
+    account: "Etsy",
+    svgIcon: "",
+    link: "https://www.etsy.com/shop/stickyschedule",
+    color: "#F1651F",
+  },
+  {
+    account: "Twitter",
+    svgIcon: "",
+    link: "https://twitter.com/andapaige",
+    color: "#1DA1F2",
+  },
+  {
+    account: "Pinterest",
+    svgIcon: "",
+    link: "https://www.pinterest.com/luxandapaige",
+    color: "#E60023",
+  },
 ];
 const affiliateLinks = [
   {
@@ -13,9 +38,9 @@ const affiliateLinks = [
   },
   { title: "", description: "", link: "" },
 ];
-function socialButtons () {
-const button = document.createElement("div");
-button.classList.add("socialButtons")
-const svg = document.createElement("img");
-const link = document.createElement("a");
+function socialButtons() {
+  const button = document.createElement("div");
+  button.classList.add("socialButtons");
+  const svg = document.createElement("img");
+  const link = document.createElement("a");
 }
