7ec7039b647b3a6a33537f5ddc954b5290a1574c
Amanda Nelson <amanda.gaylord@gmail.com>
2020-10-13 16:59:30 -0400

    basic styling added to index.css and disclosure text content updated in index.html. portfolio.html also created as a landing page for the portfolio for now.

---
+++ index.js
@@ -0,0 +1,15 @@
+const socialLinks = [
+  { account: "", svgIcon: "", link: "" },
+  { account: "", svgIcon: "", link: "" },
+  { account: "", svgIcon: "", link: "" },
+  { account: "", svgIcon: "", link: "" },
+  { account: "", svgIcon: "", link: "" },
+];
+const affiliateLinks = [
+  {
+    title: "Ally Light",
+    description: "The green portable, migraine-fighting light",
+    link: "",
+  },
+  { title: "", description: "", link: "" },
+];
